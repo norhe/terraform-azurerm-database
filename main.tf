f04507ca24e2ab8dce9136dbcad5941d66d92e91
Ehron <ehron@ehron.org>
2020-03-17 20:34:57 -0400

    dont create resource group

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
-# provider "azurerm" {
-#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
-#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
-#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
-#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
-# }
-
-resource "azurerm_resource_group" "rg" {
+data "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
 }
 
 resource "azurerm_sql_database" "db" {
   name                             = "${var.db_name}"
-  resource_group_name              = "${azurerm_resource_group.rg.name}"
+  resource_group_name              = "${data.azurerm_resource_group.rg.name}"
   location                         = "${var.location}"
   edition                          = "${var.db_edition}"
   collation                        = "${var.collation}"
@@ -24,7 +17,7 @@ resource "azurerm_sql_database" "db" {
 
 resource "azurerm_sql_server" "server" {
   name                         = "${var.db_name}-sqlsvr"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  resource_group_name          = "${data.azurerm_resource_group.rg.name}"
   location                     = "${var.location}"
   version                      = "${var.server_version}"
   administrator_login          = "${var.sql_admin_username}"
@@ -34,7 +27,7 @@ resource "azurerm_sql_server" "server" {
 
 resource "azurerm_sql_firewall_rule" "fw" {
   name                = "${var.db_name}-fwrules"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
   server_name         = "${azurerm_sql_server.server.name}"
   start_ip_address    = "${var.start_ip_address}"
   end_ip_address      = "${var.end_ip_address}"
